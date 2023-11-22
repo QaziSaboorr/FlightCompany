f59a82823c5e52e2dea4089fad5af72aba0c088f
gillhabs <gillian.habermehl@ucalgary.ca>
2023-11-21 20:54:22 -0700

    Adding variables and constructors

--- implementation/backend/javaImplementation/ticket.java
+++ implementation/backend/javaImplementation/ticket.java
@@ -1,5 +1,8 @@
 package implementation.backend.javaImplementation;
 
 public class ticket {
-    
+    protected destination dest;
+    protected aircraft plane;
+    protected crew[] crews;
+    protected seat seat;
 }
