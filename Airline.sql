d872d64e7909b0368794f6a967d7efa0fc3bb171
Anfaal Mahbub <anfaal.mahbub@ucalgary.ca>
2023-11-25 14:36:33 -0700

    Update Airline.sql

--- Airline.sql
+++ Airline.sql
@@ -11,9 +11,13 @@ CREATE TABLE IF NOT EXISTS Users (
     Email VARCHAR(255),
     Password VARCHAR(255),
     Address VARCHAR(255),
+    IsMember BOOLEAN DEFAULT FALSE, -- New column for membership status
+    HasCompanyCreditCard BOOLEAN DEFAULT FALSE, -- New column for company credit card status
+    HasRedeemedCompanionTicket BOOLEAN DEFAULT FALSE, -- New column for companion ticket redemption status
     INDEX(UserName),
     INDEX(Email)
 );
+
 
 -- Flights Table
 CREATE TABLE IF NOT EXISTS Flights (
@@ -113,163 +117,159 @@ CREATE TABLE IF NOT EXISTS Passengers (
 
 -- -- Data Insert 
 
--- -- Insert fake data into the Aircrafts table
--- INSERT INTO Aircrafts (AircraftNumber) VALUES
---     ('Boeing 737'),   
---     ('Airbus A320'),  
---     ('Boeing 747'),   
---     ('Airbus A380'), 
---     ('Boeing 777');   
+-- Insert fake data into the Aircrafts table
+INSERT INTO Aircrafts (AircraftNumber) VALUES
+    ('Boeing 737'),   
+    ('Airbus A320'),  
+    ('Boeing 747'),   
+    ('Airbus A380'), 
+    ('Boeing 777');   
 
--- -- Display the contents of the Aircrafts table
--- SELECT * FROM Aircrafts;
+-- Display the contents of the Aircrafts table
+SELECT * FROM airlinedb.Aircrafts;
 
+-- Insert fake data into the Crews table (total crews for each flight)
+INSERT INTO Crews (Name, FlightID) VALUES
+    ('Sky Explorers', 1),   
+    ('Galactic Navigators', 2), 
+    ('Dream Flyers', 3),     
+    ('Skywalkers', 4),       
+    ('Cloud Surfers', 5);    
 
--- -- Insert fake data into the Crews table (total crews for each flight)
--- INSERT INTO Crews (Name, FlightID) VALUES
---     ('Sky Explorers', 1),   
---     ('Galactic Navigators', 2), 
---     ('Dream Flyers', 3),     
---     ('Skywalkers', 4),       
---     ('Cloud Surfers', 5);    
-
--- -- Display the contents of the Crews table
--- SELECT * FROM Crews;
-
-
--- -- Insert unique destinations from the Flights table
--- -- Check the contents of the Destinations table
--- INSERT INTO Destinations (DestinationName)
--- SELECT DISTINCT Destination
--- FROM (
---     SELECT Destination FROM Flights
--- ) AS AllDestinations;
-
--- -- Check the contents of the Destinations table
--- SELECT * FROM Destinations;
--- -- Inserting fake data into the Seats table for a plane with 4 seats per row for multiple flights
--- INSERT INTO Seats (FlightID, SeatNumber, SeatType, SeatPrice) VALUES
---     -- Seats for Flight ID 1
---     (1, 'A1', 'Business-Class', 200.00),
---     (1, 'A2', 'Business-Class', 200.00),
---     (1, 'A3', 'Business-Class', 200.00),
---     (1, 'A4', 'Business-Class', 200.00),
---     (1, 'B1', 'Business-Class', 200.00),
---     (1, 'B2', 'Business-Class', 200.00),
---     (1, 'B3', 'Business-Class', 100.00),
---     (1, 'B4', 'Business-Class', 100.00),
---     (1, 'C1', 'Regular', 100.00),
---     (1, 'C2', 'Regular', 100.00),
---     (1, 'C3', 'Regular', 100.00),
---     (1, 'C4', 'Regular', 100.00),
---     (1, 'D1', 'Regular', 100.00),
---     (1, 'D2', 'Regular', 100.00),
---     (1, 'D3', 'Regular', 100.00),
---     (1, 'D4', 'Regular', 100.00),
-
---     -- Seats for Flight ID 2
---     (2, 'A1', 'Business-Class', 200.00),
---     (2, 'A2', 'Business-Class', 200.00),
---     (2, 'A3', 'Business-Class', 200.00),
---     (2, 'A4', 'Business-Class', 200.00),
---     (2, 'B1', 'Business-Class', 200.00),
---     (2, 'B2', 'Business-Class', 200.00),
---     (2, 'B3', 'Business-Class', 100.00),
---     (2, 'B4', 'Business-Class', 100.00),
---     (2, 'C1', 'Regular', 100.00),
---     (2, 'C2', 'Regular', 100.00),
---     (2, 'C3', 'Regular', 100.00),
---     (2, 'C4', 'Regular', 100.00),
---     (2, 'D1', 'Regular', 100.00),
---     (2, 'D2', 'Regular', 100.00),
---     (2, 'D3', 'Regular', 100.00),
---     (2, 'D4', 'Regular', 100.00),
-
---     -- Seats for Flight ID 3
---     (3, 'A1', 'Business-Class', 200.00),
---     (3, 'A2', 'Business-Class', 200.00),
---     (3, 'A3', 'Business-Class', 200.00),
---     (3, 'A4', 'Business-Class', 200.00),
---     (3, 'B1', 'Business-Class', 200.00),
---     (3, 'B2', 'Business-Class', 200.00),
---     (3, 'B3', 'Business-Classr', 100.00),
---     (3, 'B4', 'Business-Class', 100.00),
---     (3, 'C1', 'Regular', 100.00),
---     (3, 'C2', 'Regular', 100.00),
---     (3, 'C3', 'Regular', 100.00),
---     (3, 'C4', 'Regular', 100.00),
---     (3, 'D1', 'Regular', 100.00),
---     (3, 'D2', 'Regular', 100.00),
---     (3, 'D3', 'Regular', 100.00),
---     (3, 'D4', 'Regular', 100.00),
-
--- 	-- Seats for Flight ID 4
--- 	(4, 'A1', 'Business-Class', 200.00),
--- 	(4, 'A2', 'Business-Class', 200.00),
--- 	(4, 'A3', 'Business-Class', 200.00),
--- 	(4, 'A4', 'Business-Class', 200.00),
--- 	(4, 'B1', 'Business-Class', 200.00),
--- 	(4, 'B2', 'Business-Class', 200.00),
--- 	(4, 'B3', 'Business-Class', 100.00),
--- 	(4, 'B4', 'Business-Class', 100.00),
--- 	(4, 'C1', 'Regular', 100.00),
--- 	(4, 'C2', 'Regular', 100.00),
--- 	(4, 'C3', 'Regular', 100.00),
--- 	(4, 'C4', 'Regular', 100.00),
--- 	(4, 'D1', 'Regular', 100.00),
--- 	(4, 'D2', 'Regular', 100.00),
--- 	(4, 'D3', 'Regular', 100.00),
--- 	(4, 'D4', 'Regular', 100.00),
-
--- 	-- Seats for Flight ID 5
--- 	(5, 'A1', 'Business-Class', 200.00),
--- 	(5, 'A2', 'Business-Class', 200.00),
--- 	(5, 'A3', 'Business-Class', 200.00),
--- 	(5, 'A4', 'Business-Class', 200.00),
--- 	(5, 'B1', 'Business-Class', 200.00),
--- 	(5, 'B2', 'Business-Class', 200.00),
--- 	(5, 'B3', 'Business-Class', 100.00),
--- 	(5, 'B4', 'Business-Class', 100.00),
--- 	(5, 'C1', 'Regular', 100.00),
--- 	(5, 'C2', 'Regular', 100.00),
--- 	(5, 'C3', 'Regular', 100.00),
--- 	(5, 'C4', 'Regular', 100.00),
--- 	(5, 'D1', 'Regular', 100.00),
--- 	(5, 'D2', 'Regular', 100.00),
--- 	(5, 'D3', 'Regular', 100.00),
--- 	(5, 'D4', 'Regular', 100.00);
+-- Display the contents of the Crews table
+SELECT * FROM airlinedb.crews;
 
 
+-- Insert unique destinations from the Flights table
+-- Check the contents of the Destinations table
+INSERT INTO Destinations (DestinationName)
+SELECT DISTINCT Destination
+FROM (
+    SELECT Destination FROM Flights
+) AS AllDestinations;
 
+-- Check the contents of the Destinations table
+SELECT * FROM Destinations;
+-- Inserting fake data into the Seats table for a plane with 4 seats per row for multiple flights
+INSERT INTO Seats (FlightID, SeatNumber, SeatType, SeatPrice) VALUES
+    -- Seats for Flight ID 1
+    (1, 'A1', 'Business-Class', 200.00),
+    (1, 'A2', 'Business-Class', 200.00),
+    (1, 'A3', 'Business-Class', 200.00),
+    (1, 'A4', 'Business-Class', 200.00),
+    (1, 'B1', 'Business-Class', 200.00),
+    (1, 'B2', 'Business-Class', 200.00),
+    (1, 'B3', 'Business-Class', 100.00),
+    (1, 'B4', 'Business-Class', 100.00),
+    (1, 'C1', 'Regular', 100.00),
+    (1, 'C2', 'Regular', 100.00),
+    (1, 'C3', 'Regular', 100.00),
+    (1, 'C4', 'Regular', 100.00),
+    (1, 'D1', 'Regular', 100.00),
+    (1, 'D2', 'Regular', 100.00),
+    (1, 'D3', 'Regular', 100.00),
+    (1, 'D4', 'Regular', 100.00),
 
--- SELECT * FROM airlinedb.seats;
+    -- Seats for Flight ID 2
+    (2, 'A1', 'Business-Class', 200.00),
+    (2, 'A2', 'Business-Class', 200.00),
+    (2, 'A3', 'Business-Class', 200.00),
+    (2, 'A4', 'Business-Class', 200.00),
+    (2, 'B1', 'Business-Class', 200.00),
+    (2, 'B2', 'Business-Class', 200.00),
+    (2, 'B3', 'Business-Class', 100.00),
+    (2, 'B4', 'Business-Class', 100.00),
+    (2, 'C1', 'Regular', 100.00),
+    (2, 'C2', 'Regular', 100.00),
+    (2, 'C3', 'Regular', 100.00),
+    (2, 'C4', 'Regular', 100.00),
+    (2, 'D1', 'Regular', 100.00),
+    (2, 'D2', 'Regular', 100.00),
+    (2, 'D3', 'Regular', 100.00),
+    (2, 'D4', 'Regular', 100.00),
 
+    -- Seats for Flight ID 3
+    (3, 'A1', 'Business-Class', 200.00),
+    (3, 'A2', 'Business-Class', 200.00),
+    (3, 'A3', 'Business-Class', 200.00),
+    (3, 'A4', 'Business-Class', 200.00),
+    (3, 'B1', 'Business-Class', 200.00),
+    (3, 'B2', 'Business-Class', 200.00),
+    (3, 'B3', 'Business-Classr', 100.00),
+    (3, 'B4', 'Business-Class', 100.00),
+    (3, 'C1', 'Regular', 100.00),
+    (3, 'C2', 'Regular', 100.00),
+    (3, 'C3', 'Regular', 100.00),
+    (3, 'C4', 'Regular', 100.00),
+    (3, 'D1', 'Regular', 100.00),
+    (3, 'D2', 'Regular', 100.00),
+    (3, 'D3', 'Regular', 100.00),
+    (3, 'D4', 'Regular', 100.00),
 
--- -- Inserting additional fake data into the Flights table
--- INSERT INTO Flights (FlightNumber, Origin, Destination, AircraftID) VALUES
---     ('AC222', 'Vancouver', 'Edmonton', 1),
---     ('AC333', 'Calgary', 'Ottawa', 2),
---     ('AC555', 'Ottawa', 'Winnipeg', 1),
---     ('AC777', 'Quebec City', 'Halifax', 3),
---     ('AC999', 'Winnipeg', 'Montreal', 2);
+	-- Seats for Flight ID 4
+	(4, 'A1', 'Business-Class', 200.00),
+	(4, 'A2', 'Business-Class', 200.00),
+	(4, 'A3', 'Business-Class', 200.00),
+	(4, 'A4', 'Business-Class', 200.00),
+	(4, 'B1', 'Business-Class', 200.00),
+	(4, 'B2', 'Business-Class', 200.00),
+	(4, 'B3', 'Business-Class', 100.00),
+	(4, 'B4', 'Business-Class', 100.00),
+	(4, 'C1', 'Regular', 100.00),
+	(4, 'C2', 'Regular', 100.00),
+	(4, 'C3', 'Regular', 100.00),
+	(4, 'C4', 'Regular', 100.00),
+	(4, 'D1', 'Regular', 100.00),
+	(4, 'D2', 'Regular', 100.00),
+	(4, 'D3', 'Regular', 100.00),
+	(4, 'D4', 'Regular', 100.00),
 
-
--- SELECT * FROM airlinedb.flights;
-
--- -- -- Inserting fake data into the Users table
--- INSERT INTO Users (UserName, UserType, Email, Password, Address) VALUES
---     ('JohnDoe', 'Registered', 'john.doe@example.com', NULL, '123 Main Street'),
---     ('JaneSmith', 'Airline Agent', 'jane.smith@example.com', 'agentpass', NULL),
---     ('Admin123', 'System Admin', 'admin@example.com', 'adminPass', NULL),
---     ('FlightAttendant', 'Flight Attendant', 'flyattend@example.com', 'flyattendpass', NULL),
---     ('UnregisteredUser', 'Unregistered', 'unregistered@example.com', NULL, '245 Fake Street');
-
--- SELECT * FROM airlinedb.users;
+	-- Seats for Flight ID 5
+	(5, 'A1', 'Business-Class', 200.00),
+	(5, 'A2', 'Business-Class', 200.00),
+	(5, 'A3', 'Business-Class', 200.00),
+	(5, 'A4', 'Business-Class', 200.00),
+	(5, 'B1', 'Business-Class', 200.00),
+	(5, 'B2', 'Business-Class', 200.00),
+	(5, 'B3', 'Business-Class', 100.00),
+	(5, 'B4', 'Business-Class', 100.00),
+	(5, 'C1', 'Regular', 100.00),
+	(5, 'C2', 'Regular', 100.00),
+	(5, 'C3', 'Regular', 100.00),
+	(5, 'C4', 'Regular', 100.00),
+	(5, 'D1', 'Regular', 100.00),
+	(5, 'D2', 'Regular', 100.00),
+	(5, 'D3', 'Regular', 100.00),
+	(5, 'D4', 'Regular', 100.00);
 
 
 
 
+SELECT * FROM airlinedb.seats;
 
 
+-- Inserting additional fake data into the Flights table
+INSERT INTO Flights (FlightNumber, Origin, Destination, AircraftID) VALUES
+    ('AC222', 'Vancouver', 'Edmonton', 1),
+    ('AC333', 'Calgary', 'Ottawa', 2),
+    ('AC555', 'Ottawa', 'Winnipeg', 1),
+    ('AC777', 'Quebec City', 'Halifax', 3),
+    ('AC999', 'Winnipeg', 'Montreal', 2);
 
 
+SELECT * FROM airlinedb.flights;
+
+-- -- Inserting fake data into the Users table
+
+INSERT INTO Users (UserName, UserType, Email, Password, Address, IsMember, HasCompanyCreditCard, HasRedeemedCompanionTicket) VALUES
+	('John Doe', 'Registered', 'john.doe@example.com', 'regispass', '123 Main Street', TRUE, FALSE, FALSE),
+	('Jane Smith', 'AirlineAgent', 'jane.smith@example.com', 'agentpass', NULL, NULL, NULL, NULL),
+	('Admin123', 'SystemAdmin', 'admin@example.com', 'adminPass', NULL, NULL, NULL, NULL),
+	('Flight Attendant', 'FlightAttendant', 'flyattend@example.com', 'flyattendpass', NULL, NULL, NULL, NULL),
+	('Unregistered User', 'Unregistered', 'unregistered@example.com', NULL, '245 Fake Street', FALSE, FALSE, FALSE);
+
+
+SELECT * FROM airlinedb.users;
+SELECT * FROM airlinedb.tickets;
+SELECT * FROM airlinedb.passengers;
+SELECT * FROM airlinedb.crews;
